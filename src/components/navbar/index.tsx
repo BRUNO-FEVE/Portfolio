1fdbe02f86838085b9fc0d681e559857d37c5994
Bruno Augusto Lopes Fevereiro <75047754+BRUNO-FEVE@users.noreply.github.com>
2023-06-28 23:35:27 -0300

    added fonts

---
+++ src/components/navbar/index.tsx
@@ -0,0 +1,11 @@
+import "./styles.css"
+
+function navbar() {
+    return (
+        <>
+            <h1>Teste</h1>
+        </>
+    )
+}
+
+export default navbar;
---
+++ src/pages/main_page/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './main_page.css';
+import NavBar from '../../components/navbar/index';
+
+function main_page() {
+  return (
+    <>
+    <NavBar />
+    </>
+  );
+}
+
+export default main_page;
