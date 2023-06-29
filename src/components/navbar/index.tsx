686d23ff8322633a9f41194f29f074a335d547fb
Bruno Augusto Lopes Fevereiro <75047754+BRUNO-FEVE@users.noreply.github.com>
2023-06-29 00:05:15 -0300

    mock navbar done

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -2,9 +2,14 @@ import "./styles.css"
 
 function navbar() {
     return (
-        <>
-            <h1>Teste</h1>
-        </>
+        <div className="navbar">
+            <p>Coded & Desinged by Bruno</p>
+            <ul className="left-side">
+                <li><p>Projects</p></li>
+                <li><p>Sobre Mim</p></li>
+                <li><p>Contato</p></li>
+            </ul>
+        </div>
     )
 }
 
