078586318e8bbcd88213a875c47b0b11008a5e78
Bruno Augusto Lopes Fevereiro <75047754+BRUNO-FEVE@users.noreply.github.com>
2023-06-30 09:01:40 -0300

    Marquee animation done

--- src/pages/main_page/page.tsx
+++ src/pages/main_page/page.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import './main_page.css';
 import NavBar from '../../components/navbar/index';
 import img from '../../assets/retrato-sem-fundo.png'
+import Marquee from 'react-fast-marquee';
 
 function main_page() {
   return (
@@ -12,7 +13,7 @@ function main_page() {
           <p>Estudante <br/> Engenharia de Computação & Front End</p>
           <p className='location'>Localizado na Zona Sul de São Paulo</p>
         </div>
-          <h1>- Bruno Fevereiro - Bruno Fevereiro -</h1>
+        <Marquee className='test'><h1>Bruno Fevereiro-</h1></Marquee>
           <img src={img} alt="Bruno Fevereiro"/>
       </div>
     </>
