0202c92d35e9fffd591ff5e4d96650dcaf7bcf94
Bruno Augusto Lopes Fevereiro <75047754+BRUNO-FEVE@users.noreply.github.com>
2023-07-01 19:52:21 -0300

    second section done

--- src/pages/main_page/page.tsx
+++ src/pages/main_page/page.tsx
@@ -3,6 +3,7 @@ import './main_page.css';
 import NavBar from '../../components/navbar/index';
 import img from '../../assets/retrato-sem-fundo.png'
 import Marquee from 'react-fast-marquee';
+import { HiOutlineArrowLongRight } from 'react-icons/hi2'
 
 function main_page() {
   return (
@@ -11,10 +12,17 @@ function main_page() {
       <div className='first-section'>
         <div className='background-text'>
           <p>Estudante <br/> Engenharia de Computação & Front End</p>
-          <p className='location'>Localizado na Zona Sul de São Paulo</p>
+          <p>Localizado na Zona Sul de São Paulo</p>
         </div>
-        <Marquee className='test'><h1>Bruno Fevereiro-</h1></Marquee>
+          <Marquee className='marquee'><h1>Bruno Fevereiro</h1></Marquee>
           <img src={img} alt="Bruno Fevereiro"/>
+      </div>
+      <div className='second-section'>
+        <p className='main-phrase'>Dando emoção a simples displays. Juntos podemos criar interfaces de usuário incríveis e intuitivas que proporcionam uma experiência de usuário excepcional.</p>
+        <div className='about-me'>
+          <p>Sou estudante de Engenharia da Computação no 3º ano no Instituto Mauá de Tecnologia em São Paulo. Tenho um grande interesse em desenvolvimento e design.</p>
+          <button>Sobre Mim<HiOutlineArrowLongRight className='arrow-icon'/></button>
+        </div>
       </div>
     </>
   );
