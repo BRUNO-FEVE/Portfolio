1353f778a74a10bdd1047e226989a3e5e456c4b9
Bruno Augusto Lopes Fevereiro <75047754+BRUNO-FEVE@users.noreply.github.com>
2023-08-05 20:28:30 -0300

    hour counter goal section done

--- src/components/project_modal/index.tsx
+++ src/components/project_modal/index.tsx
@@ -1,9 +1,34 @@
 import Navbar from '../navbar'
 import './styles.css'
-import { FaGithub, FaJava} from 'react-icons/fa'
+import { FaGithub, FaJava, FaPager} from 'react-icons/fa'
+import { TbBulbFilled } from 'react-icons/tb'
+import { PiFileSqlFill } from 'react-icons/pi'
+
+import IconComponent from '../little_components/icon-component'
+
 // import { computerOne } from "../../assets/1-pc-hourcounter.png";
 
 function ProjectModal(props: {bg_color: string}) {
+
+    const iconsList = [
+        {
+            icon: TbBulbFilled,
+            label: 'Tema Livre'
+        },
+        {
+            icon: PiFileSqlFill,
+            label: 'Conexão ao Banco de dados MySQL'
+        },
+        {
+            icon: FaJava,
+            label: 'Liguagem de Programação: Java'
+        },
+        {
+            icon: FaPager,
+            label: 'Interfaces Graficas'
+        }
+    ]
+
     return (
         <>
             <Navbar text={'Ficha Técnica'} text_color_black={false} bg_color={props.bg_color}/>
@@ -17,6 +42,13 @@ function ProjectModal(props: {bg_color: string}) {
             <div className="container-objective">
                 <p className='objective-title'>Principais Objetivos:</p>
                 <p className='objective-text'>O Hour Counter foi concebido como um projeto semestral da disciplina de Linguagens de Programação, com o intuito de atender a uma série de requisitos específicos. </p>
+                <div className='objective-icon-list'>
+                    {iconsList.map((icon) => {
+                        return <div>
+                            <IconComponent label={icon.label} icon={icon.icon} />
+                        </div>
+                    })}
+                </div>
             </div>
         </>
     )
