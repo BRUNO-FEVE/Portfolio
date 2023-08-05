6301f2c3986cf5a4923d752f1492c85e3fa817f2
Bruno Augusto Lopes Fevereiro <75047754+BRUNO-FEVE@users.noreply.github.com>
2023-08-05 19:39:23 -0300

    hour counter goal text done

--- src/components/project_modal/index.tsx
+++ src/components/project_modal/index.tsx
@@ -7,12 +7,16 @@ function ProjectModal(props: {bg_color: string}) {
     return (
         <>
             <Navbar text={'Ficha Técnica'} text_color_black={false} bg_color={props.bg_color}/>
-            <div className='container'>
+            <div className='container-display'>
                 <div className='display-img hour-counter-img'>
                     <p>Hour Counter</p>
                     <a href="https://github.com/BRUNO-FEVE/Hour-Counter" target={'_blank'} rel="noreferrer"><FaGithub className='github-icon'/></a>
                 </div>  
                 <FaJava className='background-icon'/>
+            </div>
+            <div className="container-objective">
+                <p className='objective-title'>Principais Objetivos:</p>
+                <p className='objective-text'>O Hour Counter foi concebido como um projeto semestral da disciplina de Linguagens de Programação, com o intuito de atender a uma série de requisitos específicos. </p>
             </div>
         </>
     )
