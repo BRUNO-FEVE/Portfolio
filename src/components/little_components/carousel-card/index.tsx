ab17ca59d589c9eb4685c62906558f9f74408c7c
Bruno Augusto Lopes Fevereiro <75047754+BRUNO-FEVE@users.noreply.github.com>
2023-08-06 14:35:24 -0300

    temporary carousel animation

--- src/components/little_components/carousel-card/index.tsx
+++ src/components/little_components/carousel-card/index.tsx
@@ -1,4 +1,5 @@
-import React, { ReactNode } from 'react'
+import { motion } from 'framer-motion'
+import React, { ReactNode, useEffect, useState } from 'react'
 import './styles.css'
 
 function CarouselCard(props: {children: ReactNode}) {
@@ -9,4 +10,47 @@ function CarouselCard(props: {children: ReactNode}) {
   )
 }
 
-export default CarouselCard
+function CarouselCardAnimation(props: {children: ReactNode, index: number, checkedIndex: number}) {
+  const [isAnimationComplete, setAnimationComplete] = useState(false)
+
+  const variants = {
+    hidden: { opacity: 1, x: window.innerWidth},
+    visible: {opacity: 1, x: 0},
+    exit: {opacity: 1 ,x: -window.innerWidth}
+  }
+
+  const setTransitionDirection = (index: number) => {
+    if(index < props.checkedIndex) {
+        return true
+    } else if(props.checkedIndex < index) {
+        return false
+    }
+  }
+
+  const handleAnimationComplete = () => {
+    if(props.index !== props.checkedIndex) {
+      setAnimationComplete(true)
+    }
+  }
+
+  useEffect(() => {
+    if(props.index === props.checkedIndex) {
+      setAnimationComplete(false)
+    }
+  }, [props.checkedIndex, props.index])
+
+  return <motion.div
+    initial={'hidden'}
+    animate={props.checkedIndex === props.index ? 'visible' : setTransitionDirection(props.index) ? 'exit' : 'hidden' }
+    transition={{duration: 0.7}}
+    variants={variants}
+    onAnimationComplete={handleAnimationComplete}
+    className={isAnimationComplete ? 'none' : ''}
+  >
+    <CarouselCard>
+      {props.children}
+    </CarouselCard>
+  </motion.div>
+}
+
+export { CarouselCard, CarouselCardAnimation } 
